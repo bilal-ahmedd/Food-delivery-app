be6738dc4d00bab63f100a465349deffaed30f91
bilal-ahmedd <bilalahmed35100@gmail.com>
2024-11-09 16:53:44 +0500

    Created add to cart functionality using useContext

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -11,7 +11,7 @@ const FoodDisplay = (category) => {
         <h2> Top dishes near you </h2>
         <div className='food-display-list'> 
           {food_list.map((item, index)=> {
-            return <FoodItem key={index} id={item.id} name={item.name} description={item.description} image={item.image} price={item.price} />
+            return <FoodItem key={index} id={item._id} name={item.name} description={item.description} image={item.image} price={item.price} />
           })}
         </div>
     </div>
