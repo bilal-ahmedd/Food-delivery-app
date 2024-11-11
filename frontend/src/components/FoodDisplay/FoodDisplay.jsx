be91c4b04d7ebde98a95ad20c1a5397384a3619f
bilal-ahmedd <bilalahmed35100@gmail.com>
2024-11-11 21:31:36 +0500

    Created the footer

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -4,14 +4,17 @@ import { StoreContext } from '../../context/StoreContext'
 import { food_list } from '../../assets/assets'
 import FoodItem from '../FoodItem/FoodItem'
 
-const FoodDisplay = (category) => {
+const FoodDisplay = ({category}) => {
     const { foodList } = useContext(StoreContext)
   return (
     <div className='food-display' id='food-display' >
         <h2> Top dishes near you </h2>
         <div className='food-display-list'> 
           {food_list.map((item, index)=> {
-            return <FoodItem key={index} id={item._id} name={item.name} description={item.description} image={item.image} price={item.price} />
+            if ( category === "All" || category === item.category ) {
+
+              return <FoodItem key={index} id={item._id} name={item.name} description={item.description} image={item.image} price={item.price} />
+            }
           })}
         </div>
     </div>
